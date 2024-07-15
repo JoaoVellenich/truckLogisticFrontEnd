f42c5b358cf3d90bb18bc21c2f675a64f89aa758
JoaoVellenich <jpvellenich@gmail.com>
2024-07-15 11:25:20 -0300

    feat: function to call register user api

---
+++ src/services/api/userApi.ts
@@ -0,0 +1,26 @@
+import { AxiosError } from "axios";
+import { backendApi } from "./config";
+
+interface RegisterUserInterface {
+  name: string;
+  email: string;
+  password: string;
+  role: "ADMIN";
+  companyName: string;
+}
+
+export async function registerUser(
+  params: RegisterUserInterface
+): Promise<string> {
+  try {
+    console.log(params);
+    const response = await backendApi.post("/user/register", params);
+    console.log(response);
+    return "";
+  } catch (error: any) {
+    const errMsg = error.response.data
+      ? "Email já cadastrado"
+      : "Erro interno, tente novamente mais tarde";
+    return errMsg;
+  }
+}
