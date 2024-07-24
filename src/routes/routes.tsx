609952cb0628317c6b5618d6b4711d051a21d69b
JoaoVellenich <jpvellenich@gmail.com>
2024-07-24 14:18:06 -0300

    feat: :closed_lock_with_key: Add routes
    adding routes

---
+++ src/routes/routes.tsx
@@ -0,0 +1,32 @@
+import React, { useContext } from "react";
+import { Routes as Router, Route, Navigate, Outlet } from "react-router-dom";
+import { AuthContext } from "../context/authContext";
+import { Login } from "../pages/login";
+import { Home } from "../pages/home";
+import { SingIn } from "../pages/singin";
+
+type Props = {};
+
+const PrivateRoutes = () => {
+  const { authenticated } = useContext(AuthContext);
+
+  if (!authenticated) return <Navigate to="/" replace />;
+
+  return <Outlet />;
+};
+
+const Routes = (props: Props) => {
+  const { authenticated } = useContext(AuthContext);
+
+  return (
+    <Router>
+      <Route path="/" element={<Login />} />
+      <Route path="/singIn" element={<SingIn />} />
+      <Route element={<PrivateRoutes />}>
+        <Route path="/home" element={<Home />} />
+      </Route>
+    </Router>
+  );
+};
+
+export default Routes;
