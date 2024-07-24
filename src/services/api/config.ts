479a076efddf7b704a235f414e85fc4d37ca2d10
JoaoVellenich <jpvellenich@gmail.com>
2024-07-24 14:20:57 -0300

    feat: :wrench: Create api and login route

--- src/services/api/config.ts
+++ src/services/api/config.ts
@@ -2,4 +2,5 @@ import axios from "axios";
 
 export const backendApi = axios.create({
   baseURL: "http://localhost:8080/api/v1",
+  withCredentials: true,
 });
--- src/services/api/userApi.ts
+++ src/services/api/userApi.ts
@@ -8,18 +8,39 @@ interface RegisterUserInterface {
   companyName: string;
 }
 
+interface LoginUserInterface {
+  email: string;
+  password: string;
+}
+
 export async function registerUser(
   params: RegisterUserInterface
 ): Promise<string> {
   try {
-    console.log(params);
-    const response = await backendApi.post("/user/register", params);
-    console.log(response);
+    await backendApi.post("/user/register", params);
     return "";
   } catch (error: any) {
     const errMsg = error.response.data
       ? "Email já cadastrado"
       : "Erro interno, tente novamente mais tarde";
     return errMsg;
+  }
+}
+
+export async function loginUser(params: LoginUserInterface): Promise<boolean> {
+  try {
+    const response = await backendApi.post("/user/login", params);
+    return response.status === 200;
+  } catch (error) {
+    return false;
+  }
+}
+
+export async function validateUser(): Promise<boolean> {
+  try {
+    const response = await backendApi.get("/user/check");
+    return response.status === 200;
+  } catch (error) {
+    return false;
   }
 }
