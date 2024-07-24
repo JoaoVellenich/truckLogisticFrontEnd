90c882f96dee0aed2fab0c4a5c8ad3dd19687b1f
JoaoVellenich <jpvellenich@gmail.com>
2024-07-24 14:20:04 -0300

    feat: :closed_lock_with_key: Add login function

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,17 +2,37 @@ import { ArrowRight, KeyRound, User, UserRoundPlus } from "lucide-react";
 import { Button } from "../../components/button/button";
 import { Logo } from "../../components/logo/logo";
 import { Input } from "../../components/input/input";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { ErrorMessage } from "../../components/errorMessage/errorMessage";
+import { loginUser } from "../../services/api/userApi";
+import { AuthContext } from "../../context/authContext";
 
 export function Login() {
   const navigate = useNavigate();
 
+  const { setAuthenticated } = useContext(AuthContext);
+
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  function handleLogin() {
-    console.log(`${username} - ${password}`);
+  const [err, setErr] = useState("");
+
+  async function handleLogin() {
+    setErr("");
+    if (!username || !password) {
+      setErr("Preencha os campos");
+      return;
+    }
+
+    const response = await loginUser({ email: username, password: password });
+    if (!response) {
+      setErr("Email ou senha incorretos");
+      return;
+    }
+    console.log("Autenticado");
+    setAuthenticated(true);
+    navigate("/home");
   }
 
   function handleSingIn() {
@@ -23,6 +43,7 @@ export function Login() {
     <div className="flex flex-col gap-3 h-screen items-center justify-center content-center">
       <Logo />
       <div className="max-w-3xl w-full flex flex-col justify-center items-center space-y-2.5">
+        <ErrorMessage message={err} />
         <div className="text-lg bg-zinc-300 w-80 h-14 rounded-3xl flex justify-center items-center gap-2 p-5">
           <User className="text-zinc-700 size-5" />
           <Input
