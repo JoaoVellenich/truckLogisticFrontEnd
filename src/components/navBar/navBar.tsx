940df8cdab541110fb885ecfe82ea6ac9f97ebff
JoaoVellenich <jpvellenich@gmail.com>
2024-07-25 15:56:37 -0300

    fix: :art: Style h

--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -7,7 +7,7 @@ export function NavBar() {
   const navigate = useNavigate();
   const [currentPage, setCurrentPage] = useState(location.pathname);
   return (
-    <div className="flex flex-col bg-zinc-100 h-screen items-center justify-center gap-2 pr-2">
+    <div className="flex flex-col h-screen bg-zinc-100 items-center justify-center gap-2 pr-2">
       <NavBarButton
         title="Página principal"
         isActive={currentPage.split("/")[1] === "home"}
