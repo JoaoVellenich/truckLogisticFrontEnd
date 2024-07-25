4f9bfaeab1df678ed3a2f213f908fba5f4b3e7ee
JoaoVellenich <jpvellenich@gmail.com>
2024-07-25 13:48:26 -0300

    style: :art: adding padding to button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -2,7 +2,7 @@ import { ComponentProps, ReactNode } from "react";
 import { tv, VariantProps } from "tailwind-variants";
 
 const buttonVariants = tv({
-  base: "rounded-3xl h-14 w-full flex justify-center items-center gap-2",
+  base: "rounded-3xl h-14 w-full flex justify-center items-center gap-2 px-2",
   variants: {
     variant: {
       primary: "bg-lime-300 text-lime-950 hover:bg-lime-400",
