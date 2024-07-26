d4acef2e621011de5f39db57685157a3e9172272
JoaoVellenich <jpvellenich@gmail.com>
2024-07-26 11:04:02 -0300

    style: :art: Remove header

--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -1,6 +1,7 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { NavBarButton } from "./navBarButton/navBarButton";
 import { useState } from "react";
+import { Truck } from "lucide-react";
 
 export function NavBar() {
   const location = useLocation();
@@ -8,6 +9,12 @@ export function NavBar() {
   const [currentPage, setCurrentPage] = useState(location.pathname);
   return (
     <div className="flex flex-col h-screen bg-zinc-100 items-center justify-center gap-2 pr-2">
+      <div className="flex gap-2 bg-zinc-100 py-2 px-2 items-center">
+        <div className="flex justify-start items-center">
+          <Truck className="text-zinc-700 size-7" />
+          <h2 className="text-3xl text-zinc-900">Truckerr</h2>
+        </div>
+      </div>
       <NavBarButton
         title="Página principal"
         isActive={currentPage.split("/")[1] === "home"}
