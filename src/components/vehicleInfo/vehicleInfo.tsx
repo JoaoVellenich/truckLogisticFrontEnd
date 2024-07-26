fe90f884d0a0cd7308021ebd59bceb4de5c4aa30
JoaoVellenich <jpvellenich@gmail.com>
2024-07-26 14:02:12 -0300

    feat: :art: Create box of truck info

---
+++ src/components/vehicleInfo/vehicleInfo.tsx
@@ -0,0 +1,26 @@
+import { TruckInterface } from "../../type/truckType";
+
+interface VehicleInfoProps {
+  truck: TruckInterface;
+}
+
+export function VehicleInfo({ truck }: VehicleInfoProps) {
+  return (
+    <div className="flex flex-col w-full bg-zinc-100 rounded-3xl px-4 py-2 gap-2">
+      <div className="flex flex-1 w-full justify-between text-center items-center">
+        <span className="font-semibold">Placa</span>
+        <span>{truck.plate}</span>
+      </div>
+      <div className="w-full h-px bg-zinc-300"></div>
+      <div className="flex flex-1 w-full justify-between text-center items-center">
+        <span className="font-semibold">Descrição</span>
+        <span>{truck.description}</span>
+      </div>
+      <div className="w-full h-px bg-zinc-300"></div>
+      <div className="flex flex-1 w-full justify-between text-center items-center">
+        <span className="font-semibold">Odometro</span>
+        <span>{truck.km.toLocaleString("pt-br")}</span>
+      </div>
+    </div>
+  );
+}
