01196991602d12ef2c00af11bd3fbb0f8f883902
JoaoVellenich <jpvellenich@gmail.com>
2024-07-26 14:01:16 -0300

    feat: Create fuel table

---
+++ src/components/fuelTable/fuelCard/fuelCard.tsx
@@ -0,0 +1,42 @@
+import { Calendar, CircleDollarSign, Fuel, MapPin } from "lucide-react";
+import { FuelInterface } from "../../../type/fuel";
+
+interface FuelCardProps {
+  fuel: FuelInterface;
+}
+
+export function FuelCard({ fuel }: FuelCardProps) {
+  return (
+    <div className="flex flex-col flex-1 w-full bg-zinc-200 rounded-3xl px-4 py-2 gap-2">
+      <div className="flex justify-between">
+        <div className="flex items-center gap-2">
+          <MapPin className="size-4" />
+          <span>{fuel.location}</span>
+        </div>
+        <div className="flex items-center gap-2">
+          <Calendar className="size-4" />
+          <span>{fuel.date.split("T")[0]}</span>
+        </div>
+      </div>
+      <div className="flex justify-between">
+        <div className="flex items-center gap-2">
+          <CircleDollarSign className="size-4" />
+          <span>{fuel.value.toLocaleString("pt-br")}</span>
+        </div>
+        <div className="flex items-center gap-2">
+          <span>{fuel.trip.toLocaleString("pt-br")}</span>
+          <span>Km</span>
+        </div>
+      </div>
+      <div className="flex justify-between">
+        <div>
+          <span>R$/L {fuel.price.toLocaleString("pt-br")}</span>
+        </div>
+        <div className="flex items-center gap-2">
+          <span>{fuel.liters.toLocaleString("pt-br")}</span>
+          <Fuel className="size-4" />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/fuelTable/fuelTable.tsx
@@ -0,0 +1,120 @@
+import {
+  CirclePlus,
+  CircleX,
+  MoveLeft,
+  MoveRight,
+  ShieldAlert,
+} from "lucide-react";
+import { Button } from "../button/button";
+import { FuelCard } from "./fuelCard/fuelCard";
+import { TruckInterface } from "../../type/truckType";
+import { useEffect, useState } from "react";
+import { getFuelPage } from "../../services/api/fuel";
+import { FuelInterface } from "../../type/fuel";
+
+interface FuelTableProps {
+  truck: TruckInterface;
+}
+
+export function FuelTable({ truck }: FuelTableProps) {
+  const [fuels, setFuels] = useState<FuelInterface[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [err, setErr] = useState("");
+  const [currPage, setCurrPage] = useState(0);
+  const [maxPage, setMaxPage] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      const response = await getFuelPage(truck.id, 0, 7);
+      if (response.status == 200) {
+        const fuelsFound: FuelInterface[] = [];
+        response.data.content.map((fuel: any) => {
+          fuelsFound.push(fuel);
+        });
+        setFuels(fuelsFound);
+        setMaxPage(response.data.totalPages);
+      } else {
+        setErr("Erro tente novamente");
+      }
+      setIsLoading(false);
+    };
+    setErr("");
+    fetchData();
+  }, [truck]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      const response = await getFuelPage(truck.id, currPage, 7);
+      if (response.status == 200) {
+        const fuelsFound: FuelInterface[] = [];
+        response.data.content.map((fuel: any) => {
+          fuelsFound.push(fuel);
+        });
+        setFuels(fuelsFound);
+        setMaxPage(response.data.totalPages);
+      } else {
+        setErr("Erro tente novamente");
+      }
+      setIsLoading(false);
+    };
+    setErr("");
+    fetchData();
+  }, [currPage]);
+
+  return (
+    <div className="flex flex-1 flex-col w-full h-full bg-zinc-100 rounded-3xl px-4 py-2 gap-2">
+      <div className="flex w-full justify-between items-center text-center">
+        <span className="font-semibold">Combustivel</span>
+        <Button size="small">
+          <CirclePlus className="size-5" />
+          Cadastrar
+        </Button>
+      </div>
+      <div className="w-full h-px bg-zinc-300"></div>
+      <div className="flex flex-1 flex-col w-full overflow-y-auto">
+        {err ? (
+          <div className="flex flex-1 flex-col justify-center items-center">
+            <CircleX className="size-8" />
+            <span className="text-2xl">{err}</span>
+          </div>
+        ) : fuels.length <= 0 ? (
+          <div className="flex flex-1 flex-col justify-center items-center">
+            <ShieldAlert className="size-8" />
+            <span className="text-2xl">Nenhum abastecimento cadastrado</span>
+          </div>
+        ) : (
+          <div className="flex flex-col gap-1">
+            {fuels.map((fuel) => {
+              return <FuelCard fuel={fuel} />;
+            })}
+          </div>
+        )}
+      </div>
+      <div className="flex w-full justify-between">
+        <button
+          onClick={() => {
+            if (currPage >= 1) {
+              setCurrPage(currPage - 1);
+            }
+          }}
+        >
+          <MoveLeft />
+        </button>
+        <span>
+          {currPage + 1} / {maxPage}
+        </span>
+        <button
+          onClick={() => {
+            if (currPage < maxPage - 1) {
+              setCurrPage(currPage + 1);
+            }
+          }}
+        >
+          <MoveRight />
+        </button>
+      </div>
+    </div>
+  );
+}
