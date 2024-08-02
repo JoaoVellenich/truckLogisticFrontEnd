b78c3a5959a028630e7acfebcc9fe3a3fd80d2dd
JoaoVellenich <jpvellenich@gmail.com>
2024-08-02 14:45:48 -0300

    feat(FrontEnd): Add the modal to delete

--- src/components/vehicleInfo/vehicleInfo.tsx
+++ src/components/vehicleInfo/vehicleInfo.tsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import { TruckInterface } from "../../type/truckType";
 import { Button } from "../button/button";
+import { DeleteModal } from "./deleteModal/deleteModal";
 
 interface VehicleInfoProps {
   truck: TruckInterface;
 }
 
 export function VehicleInfo({ truck }: VehicleInfoProps) {
+  const [isOpenDeleteModal, setIsOpenDeleteModal] = useState(false);
+
   return (
     <div className="flex flex-col w-full bg-zinc-100 rounded-3xl px-4 py-2 gap-2">
       <div className="flex flex-1 w-full justify-between text-center items-center">
@@ -23,9 +27,22 @@ export function VehicleInfo({ truck }: VehicleInfoProps) {
         <span>{truck.km.toLocaleString("pt-br")}</span>
       </div>
       <div className="w-full h-px bg-zinc-300"></div>
-      <Button size="medium" variant="secondary">
+      <Button
+        size="medium"
+        variant="secondary"
+        onClick={() => {
+          setIsOpenDeleteModal(true);
+        }}
+      >
         Apagar
       </Button>
+
+      {isOpenDeleteModal && (
+        <DeleteModal
+          closeCreateVehicleModal={() => setIsOpenDeleteModal(false)}
+          plate={truck.plate}
+        />
+      )}
     </div>
   );
 }
